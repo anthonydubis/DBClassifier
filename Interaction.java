6460934b6776005156647a43ea477e4dcd2321c9
Anthony Dubis <anthonydubis@gmail.com>
2015-10-23 15:06:35 -0400

    Not retrieving the number of documents contained by a given host for a specific query.

--- Interaction.java
+++ Interaction.java
@@ -1,11 +1,17 @@
+import java.io.IOException;
+
+import org.json.JSONException;
 
 public class Interaction {
-	public static void main (String[] args) {
+	public static void main (String[] args) throws IOException, JSONException {
 		String key = args[0];
 		float t_es = Float.valueOf(args[1]);
 		int t_ec = Integer.valueOf(args[2]);
 		String host = args[3];
 		
 		System.out.println("Key: " + key + "\nt_es: " + t_es + "\nt_ec: " + t_ec + "\nhost: " + host);
+		
+		int n = Utils.getNumDocs(key, host, "cancer");
+		System.out.println("Number of docs: " + n);
 	}
 }
---
+++ Utils.java
@@ -0,0 +1,54 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.commons.codec.binary.Base64;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Utils {
+
+	public static int getNumDocs(String key, String host, String query) throws IOException, JSONException {
+		int numDocs = 0;
+		
+		/* Setup query URL */
+		query = query.replaceAll(" ", "%20");
+		String site = "site%3a" + host;
+		String bingUrl = "https://api.datamarket.azure.com/Data.ashx/Bing/SearchWeb/v1/Composite?Query=%27" + site + "%20" + query + "%27&$top=10&$format=JSON";
+		System.out.println("URL: " + bingUrl);
+
+		/* Setup account key */
+		byte[] accountKeyBytes = Base64.encodeBase64((key + ":" + key).getBytes());
+		String accountKeyEnc = new String(accountKeyBytes);
+
+		URL url = new URL(bingUrl);
+		URLConnection connection = url.openConnection();
+		connection.setRequestProperty("Authorization", "Basic " + accountKeyEnc);
+
+		JSONArray jsonArr = null;
+		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));	
+		try {
+			String line;
+			StringBuilder response = new StringBuilder();
+			while ((line = in.readLine()) != null) {
+				response.append(line);
+			}
+			final JSONObject json = new JSONObject(response.toString());
+			jsonArr = json.getJSONObject("d").getJSONArray("results");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} finally {
+			in.close();
+		}
+		
+		System.out.println(jsonArr);
+		JSONObject json = jsonArr.getJSONObject(0);
+		numDocs = json.getInt("WebTotal"); 
+		
+		return numDocs;
+	}
+}
