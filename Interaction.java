460ec39c02c4c28872ffbd0551eec28609aa5d3b
Anthony Dubis <anthonydubis@gmail.com>
2015-10-23 13:49:52 -0400

    Initial commit that reads command line arguments.

---
+++ Interaction.java
@@ -0,0 +1,12 @@
+
+public class Interaction {
+	
+	public static void main (String[] args) {
+		String key = args[0];
+		float t_es = Float.valueOf(args[1]);
+		int t_ec = Integer.valueOf(args[2]);
+		String host = args[3];
+		
+		System.out.println("Key: " + key + "\nt_es: " + t_es + "\nt_ec: " + t_ec + "\nhost: " + host);
+	}	
+}
