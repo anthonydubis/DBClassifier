4811aa889c99b5cf1fa84972cf1574d29d75dae7
JLouback <juliana.m.louback@gmail.com>
2015-10-24 19:49:34 -0400

    Refactor bing query function

--- Utils.java
+++ Utils.java
@@ -2,6 +2,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -11,16 +12,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Utils {
-
-	public static int getNumDocs(String key, String host, String query) throws IOException, JSONException {
-		int numDocs = 0;
-		
+	
+	public static JSONArray queryBing(String key, String host, String query, String top) throws IOException {
 		/* Setup query URL */
 		query = query.replaceAll(" ", "%20");
-		String site = "site%3a" + host;
-		String bingUrl = "https://api.datamarket.azure.com/Data.ashx/Bing/SearchWeb/v1/Composite?Query=%27" + site + "%20" + query + "%27&$top=10&$format=JSON";
-		//System.out.println("URL: " + bingUrl);
-
+		String urlPattern = "https://api.datamarket.azure.com/Data.ashx/Bing/SearchWeb/v1/Composite?Query=%%27site:%s%%20%s%%27&$top=%s&$format=JSON";
+		String bingUrl = String.format(urlPattern, host, query, top);
 		/* Setup account key */
 		byte[] accountKeyBytes = Base64.encodeBase64((key + ":" + key).getBytes());
 		String accountKeyEnc = new String(accountKeyBytes);
@@ -29,7 +26,7 @@ public class Utils {
 		URLConnection connection = url.openConnection();
 		connection.setRequestProperty("Authorization", "Basic " + accountKeyEnc);
 
-		JSONArray jsonArr = null;
+		JSONArray results = null;
 		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));	
 		try {
 			String line;
@@ -38,17 +35,17 @@ public class Utils {
 				response.append(line);
 			}
 			final JSONObject json = new JSONObject(response.toString());
-			jsonArr = json.getJSONObject("d").getJSONArray("results");
+			results = json.getJSONObject("d").getJSONArray("results");
 		} catch (JSONException e) {
 			e.printStackTrace();
 		} finally {
 			in.close();
 		}
-		
-		//System.out.println(jsonArr);
-		JSONObject json = jsonArr.getJSONObject(0);
-		numDocs = json.getInt("WebTotal"); 
-		//System.out.println(numDocs);
-		return numDocs;
+		return results;
+	}
+
+	public static int getNumDocs(String key, String host, String query) throws IOException, JSONException {
+		JSONArray jsonArr = queryBing(key, host, query, "10");
+		return jsonArr.getJSONObject(0).getInt("WebTotal"); 
 	}
 }
